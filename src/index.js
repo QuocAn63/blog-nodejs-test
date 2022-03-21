2784f551d49015618f18a6cd4fa08d439ff95245
Quoc An <caoan632002@gmail.com>
2022-03-21 17:10:36 +0700

    adding morgan, nodemon, prettier, lint-staged, husky, config folder with mvc modal

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const morgan = require('morgan');
+const express = require('express');
+const path = require('path');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+const news = require('./app/controllers/NewsController');
+const route = require('./routes');
+
+// app.use(morgan('combined'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Template engine
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log('Path: ', __dirname);
+
+// Routes init
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
